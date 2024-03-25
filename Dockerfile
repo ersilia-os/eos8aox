105ee2cf18c0a7c566b517cf83afd7cc7551553d
gemmaturon <gemma@ersilia.io>
2024-03-25 13:50:53 +0100

    updating readme [skip ci]

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM bentoml/model-server:0.11.0-py310
 MAINTAINER ersilia
 
-RUN pip install signaturizer3d==0.1.7
+RUN pip install signaturizer3d==0.1.8
 RUN pip install torch==2.2.1 torchvision==0.17.1  --index-url https://download.pytorch.org/whl/cpu
 
 WORKDIR /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -7,12 +7,12 @@ import json
 import csv
 from typing import List
 
-from signaturizer3d.space import CCSpace
+
+from signaturizer3d import CCSpace, Signaturizer
 
 root = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(root)
 
-from model import FineTunedUniMol
 
 # current file directory
 checkpoints_dir = os.path.join(root, "..", "..", "checkpoints")
@@ -27,39 +27,11 @@ def get_model_path(ds):
     return os.path.join(checkpoints_dir, "{}_split3.pt".format(ds))
 
 
-class Signaturizer:
-    def __init__(self, space: CCSpace, model_file_url):
-        if not isinstance(space, CCSpace):
-            try:
-                space = CCSpace[space]
-            except KeyError:
-                raise ValueError(
-                    f"{space} is not a valid CCSpace. Valid spaces are A1 ... E5, see the CCSpace enum."
-                )
-        self.space = space
-        self.model_file_url = model_file_url
-
-        # Hydrogens are always removed as the signaturizers models are fine
-        # tuned from the pre-trained UniMol model trained without hydrogens
-        self.model = FineTunedUniMol(space=self.space, remove_hs=True, use_local_weights=True, model_file_url = self.model_file_url)
-
-    def infer_from_smiles(self, smiles_list: List[str]) -> npt.NDArray[np.float32]:
-        sig_output = self.model.get_sig4_smiles(smiles_list)
-        return sig_output
-
-
-ds = "A1"
-path = get_model_path(ds)
-signaturizer = Signaturizer(space=ds, model_file_url= path)
-smiles_list = ['C', 'CCC', "CN(C)CCOC(C1=CC=CC=C1)C1=CC=CC=C1" ]
-signatures = signaturizer.infer_from_smiles(smiles_list)
-print(signatures.shape) # -> (3, 128) a 128D vector per molecule
-
 def predict(smiles_list):
     res_ = {}
     for ds in DATASETS:
         path = get_model_path(ds)
-        signaturizer = Signaturizer(space=ds, model_file_url= path)
+        signaturizer = Signaturizer(space=ds, local_weights_path= path)
         signatures = signaturizer.infer_from_smiles(smiles_list)
         res_[ds]=signatures
     result = []
