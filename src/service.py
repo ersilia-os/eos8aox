ae928f18ab4b0fb18d75bbf520f2f1c48fc851e5
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-04-17 13:07:54 +0200

    load json instead of csv

--- src/service.py
+++ src/service.py
@@ -48,7 +48,7 @@ def String(x):
 class Model(object):
     def __init__(self):
         self.DATA_FILE = "_data.csv"
-        self.OUTPUT_FILE = "_output.csv"
+        self.OUTPUT_FILE = "_output.json"
         self.RUN_FILE = "_run.sh"
         self.LOG_FILE = "run.log"
 
@@ -85,16 +85,8 @@ class Model(object):
                 cmd, stdout=fp, stderr=fp, shell=True, env=os.environ
             ).wait()
         with open(output_file, "r") as f:
-            reader = csv.reader(f)
-            h = next(reader)
-            R = []
-            for r in reader:
-                R += [
-                    {"outcome": [Float(x) for x in r]}
-                ]  # <-- EDIT: Modify according to type of output (Float, String...)
-        meta = {"outcome": h}
-        result = {"result": R, "meta": meta}
-        shutil.rmtree(tmp_folder)
+            result = json.load(f)
+        shutil.rmtree(tmp_folder)    
         return result
 
 
